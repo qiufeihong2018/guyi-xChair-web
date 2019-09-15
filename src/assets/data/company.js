d0b63e3d37567a210b24f47fa2bb5a37aa26fd29
LuMengli <mengfaluo@163.com>
2019-09-15 19:49:12 +0800

    feat(data-visual): add charts in overview

---
+++ src/assets/data/company.js
@@ -0,0 +1,114 @@
+export const companies = [
+  {
+    name: '中源家居股份有限公司',
+    abbreviation: '中源',
+    address: '安吉县递铺街道塘浦工业园区2-5幢',
+    gis: [], // 地图坐标
+    zipCode: '313300',
+    totalStaff: 1453,
+    businessScope: '家具及家居配件的生产和销售',
+    honor: '浙江省研发中心、高新技术企业、浙江省出口名牌',
+    certification: '三体系认证',
+    mainProduct: '固定沙发、功能沙发',
+    pctOfMainProduct: 0.7,
+    kpi: [
+      {
+        year: 2016, // 2016的数据
+        output: 60100, // 工业总产数值(万)
+        operatingIncome: 59174, // 营业总收入
+        sale: 59174, // 销售收入
+        export: 8125, // 出口创汇(万·美元)
+        tax: 2246, // 缴税总额
+        profitAfterTax: 7322, // 税后收入
+        averageSale: 40.73, // 人均销售收入
+        totalAsset: 37074, // 资产总额
+        fixedAsset: 9467, // 固定资产
+        debtRatio: 0.65, // 资产负债率
+      }
+    ]
+  },
+  {
+    name: '永艺家具股份有限公司',
+    abbreviation: '永艺',
+    address: '浙江省湖州市安吉县递铺镇永艺西路1号',
+    gis: [], // 地图坐标
+    zipCode: '313300',
+    totalStaff: 2608,
+    businessScope: '产品主要涉及办公椅、休闲椅、按摩椅椅身、会议椅及功能座椅配件',
+    honor: '中国外观设计优秀奖、国际发明展“发明创业奖·项目奖”金奖、浙江省科学技术进步二等奖、湖州市科学技术进步一等奖',
+    certification: '三体系认证',
+    mainProduct: '办公椅',
+    pctOfMainProduct: 0.9,
+    kpi: [
+      {
+        year: 2016, // 2016的数据
+        output: 122996, // 工业总产数值(万)
+        operatingIncome: 107194, // 营业总收入
+        sale: 107194, // 销售收入
+        export: 16013, // 出口创汇(万·美元)
+        tax: 2254, // 缴税总额
+        profitAfterTax: 9479, // 税后收入
+        averageSale: 41.10, // 人均销售收入
+        totalAsset: 73975, // 资产总额
+        fixedAsset: 7105, // 固定资产
+        debtRatio: 0.43, // 资产负债率
+      }
+    ]
+  },
+  {
+    name: '安吉富和家具有限公司',
+    abbreviation: '富和',
+    address: '安吉递铺范潭工业园区',
+    gis: [], // 地图坐标
+    zipCode: '313300',
+    totalStaff: 540,
+    businessScope: '生产家具及配件，销售本公司产品',
+    honor: '高新技术企业、省级工业设计中心、省级高新技术企业研发中心',
+    certification: '已获得质量体系认证、环境体系认证等证书',
+    mainProduct: '高端办公椅、休闲椅、特色酒吧椅',
+    pctOfMainProduct: 0.9,
+    kpi: [
+      {
+        year: 2016, // 2016的数据
+        output: 35000, // 工业总产数值(万)
+        operatingIncome: 33005, // 营业总收入
+        sale: 33005, // 销售收入
+        export: 4660, // 出口创汇(万·美元)
+        tax: 3773, // 缴税总额
+        profitAfterTax: 2528, // 税后收入
+        averageSale: 61, // 人均销售收入
+        totalAsset: 27770, // 资产总额
+        fixedAsset: 4926, // 固定资产
+        debtRatio: 0.58, // 资产负债率
+      }
+    ]
+  },
+  {
+    name: '浙江恒林椅业股份有限公司',
+    abbreviation: '恒林',
+    address: '浙江省湖州市安吉县递铺镇阳光工业园区',
+    gis: [], // 地图坐标
+    zipCode: '313300',
+    totalStaff: 2300,
+    businessScope: '生产、销售办公椅、按摩椅、家具等健康坐具产品',
+    honor: '浙江省著名商标、湖州市著名商标、浙江省出口名牌、浙江省名牌产品',
+    certification: 'ISO9001:2000,ISO14001:2004体系认证及德国GS/LGA认证',
+    mainProduct: '办公椅、按摩椅',
+    pctOfMainProduct: 0.9,
+    kpi: [
+      {
+        year: 2016, // 2016的数据
+        output: 141869, // 工业总产数值(万)
+        operatingIncome: 143231, // 营业总收入
+        sale: 143231, // 销售收入
+        export: 20305, // 出口创汇(万·美元)
+        tax: 12453, // 缴税总额
+        profitAfterTax: 21953, // 税后收入
+        averageSale: 62.27, // 人均销售收入
+        totalAsset: 89283, // 资产总额
+        fixedAsset: 11486, // 固定资产
+        debtRatio: 0.43, // 资产负债率
+      }
+    ]
+  }
+]
---
+++ src/views/data-visual/overview/IncomeBarChart.vue
@@ -0,0 +1,80 @@
+<template>
+  <div :id="id" style="height: 95%"></div>
+</template>
+
+<script>
+import resize from '@/mixins/resize'
+import { companies } from 'assets/data/company'
+import color from 'assets/data/color'
+import echarts from 'echarts'
+export default {
+  name: 'IncomeBarChart',
+  mixins: [resize],
+  props: {
+    id: {
+      type: String,
+      default: 'IncomeBarChart'
+    },
+  },
+  data() {
+    return {
+      chart: null
+    }
+  },
+  computed: {
+    option() {
+      return {
+        tooltip: {
+          formatter: '{a}: {c}万元'
+        },
+        grid: {
+          left: '15%'
+        },
+        color: color.category6,
+        xAxis: {
+          type: 'category',
+          data: companies.map(item => item.abbreviation),
+          axisLabel: {
+            textStyle: {
+              color: '#fff'
+            }
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#fff'
+            }
+          },
+        },
+        yAxis: {
+          type: 'value',
+          name: '单位：万元',
+          axisLabel: {
+            textStyle: {
+              color: '#fff'
+            }
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#fff'
+            }
+          },
+        },
+        series: [{
+          name: '营业总收入',
+          data: companies.map(item => item.kpi[0].operatingIncome),
+          type: 'bar',
+        },
+        {
+          name: '销售收入',
+          data: companies.map(item => item.kpi[0].sale),
+          type: 'bar',
+        }]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
