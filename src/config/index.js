9923e87a1cbd71f387057e312c1e3760c6c0493d
lula <bodanli159951@163.com>
2019-09-20 10:15:49 +0800

    refactor: 优化代码

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   company: '杭州谷逸网络科技',
-  baseUrl: process.env.NODE_ENV === 'production' ? 'http://192.168.1.246:5001/api/v1/' : '',
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://121.40.107.95:5001/api/v1/' : 'http://localhost:5001/api/v1/',
   layout: {
     type: 'default', // 左右布局: default; T型布局: t-type; 上下布局: vertical
     maxWidth: '170px',
